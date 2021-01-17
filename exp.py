14be6092733bd0b517bd2fc9e4583da143f0c5ee
JoakimOscarsson <oscarsson.joakim@gmail.com>
2021-01-17 22:50:22 +0100

    Refactored GameObj to support separation of app_data and save_data

--- exp.py
+++ exp.py
@@ -4,93 +4,113 @@ import random
 
 def main():
     stream = open("data.yaml", "r")
-    d = yaml.load(stream)
-    sets = d["sets"]
-    game = GameObj(sets)
-    teams = game.find_teams(4)
-    print(teams)
+    app_data = yaml.load(stream)
+    stream.close()
+
+    stream = open("save.yaml", "r") 
+    save_data = yaml.load(stream)
+    stream.close()
+
+    game = GameObj(app_data, save_data)
+    print(game.make_teams(4))
+
+    
+    
+    #game = GameObj(sets)
+    #teams = game.find_teams(4)
+    #print(teams)
 
 
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+class GameObj():
+    def __init__(self, raw_app_data, raw_save_data):
+        # 0) pre-process raw data
+        set_data = raw_app_data["sets"]
+        available_set_names = raw_save_data["available sets"]
+        disabled_factions = raw_save_data["disabled factions"]
+        # 1) Make list of all sets, where the sets contain information of their respective factions
+        self.supported_sets = self._init_supported_set_data(set_data)
+        # 2) Make list of all available sets with references to the list of all sets
+        self.available_sets = self._init_available_sets(available_set_names)
+        # #) Put all factions of the active sets in a list, as long as they are not in the list of names of disabled factions
+        self.enabled_factions = self._init_enabled_factions(disabled_factions)
 
-
-class GameObj(metaclass=Singleton):
-    def __init__(self, set_list) -> None:
-        super().__init__()
-        self.sets = []
-        for set_dict in set_list:
-            self.sets.append(Set(self, set_dict))
-        self.factions = []
-        for set in self.sets:
+    def _init_supported_set_data(self, loaded_data):
+        supported_sets = []
+        for set_dict in loaded_data:
+            supported_sets.append(Set(self, set_dict))           
+        return supported_sets
+    
+    def _init_available_sets(self, available_names):
+        available_sets = []
+        for set in self.supported_sets:
+            if set.name in available_names:
+                available_sets.append(set)
+        return available_sets
+    
+    def _init_enabled_factions(self, disabled_names):
+        enabled_factions = []
+        for set in self.available_sets:
             for faction in set.factions:
-                self.factions.append(faction)
-        #Set availability:
-        self.available_factions = []
-        self.unavailable_factions = []
-        for faction in self.factions:
-            if faction.enabled == True:
-                self.available_factions.append(faction)
-            else:
-                self.unavailable_factions.append(faction)        
-
-    def disable_faction(self, faction):
-        self.available_factions.pop(self.available_factions.index(faction))
-        self.unavailable_factions.append(faction)
-        faction._disable()
-
+                if faction.name not in disabled_names:
+                    enabled_factions.append(faction)
+        return enabled_factions
+    
     def enable_faction(self, faction):
-        self.available_factions.append(faction)
-        self.unavailable_factions.pop(self.unavailable_factions.index(faction))
-        faction._enable()
+        if faction not in self.enabled_factions:
+            self.enabled_factions.append(faction)
+            faction._enable()  # TODO: See if we can remove the enable-tracker in the faction class.
+    
+    def disable_faction(self, faction):
+        if faction in self.enabled_factions:
+            self.enabled_factions.pop(self.enabled_factions.index(faction))
+            faction._disable()
+    
+    def make_set_available(self, set):
+        self.available_sets.append(set)
+        for faction in set.factions:
+            self.enable_faction(faction)
+    
+    def make_set_unavailable(self, set):
+        self.available_sets.pop(self.available_sets.index(set))
+        for faction in set.factions:
+            self.disable_faction(faction)
 
     def find_faction_by_name(self, faction_name):
-        for faction in self.faction:
-            if faction.name == faction_name:
-                return faction
-        return None
-    
+        for set in self.supported_sets:
+            for faction in set.factions:
+                if faction.name == faction_name:
+                    return faction
+        raise KeyError("Faction '" + faction_name + "' was not found!")
+
     def find_set_by_name(self, set_name):
-        for set in self.sets:
+        for set in self.supported_sets:
             if set.name == set_name:
                 return set
-        return None
-    
-    def rnd_faction(self):
-        rnd = random.randrange(0, len(self.available_factions))
-        return self.available_factions[rnd]
+        raise KeyError("Set '" + set_name + "' was not found!")
 
-    def find_team(self):
-        f1 = self.rnd_faction()
+    def _get_rnd_faction(self):
+        rnd = random.randrange(0, len(self.enabled_factions))
+        return self.enabled_factions[rnd]
+
+    def _make_team(self):
+        f1 = self._get_rnd_faction()
         self.disable_faction(f1)
-        f2 = self.rnd_faction()
+        f2 = self._get_rnd_faction()
         self.disable_faction(f2)
         return (f1, f2)
-
     
-    def find_teams(self, num_of_players):
-        generator = self.__teams_gen(num_of_players)
+    def make_teams(self, num_of_teams):
+        team = 1
         teams = []
-        try:
-            while True:
-                team = next(generator)
-                teams.append(team)
-        except StopIteration:
-            pass
-        finally:
-            del generator
+        # Make teams
+        while team <= num_of_teams:
+            teams.append(self._make_team())
+            team += 1
+        # Reset factions
+        for team in teams:
+            self.enable_faction(team[0])
+            self.enable_faction(team[1])
         return teams
-        
-    
-    def __teams_gen(self, number_of_players):
-        player = 1
-        while player <= number_of_players:
-            yield self.find_team()
-            player += 1
 
 
 class Readable():
